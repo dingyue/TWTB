5734e69a065473895fa5ab1f96504b393cf3c7a7
Ding Yue <yding@thoughtworks.com>
2012-12-04 19:46:14 +0800

    added files

---
+++ TWTB/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TWTB::Application.config.secret_token = '22cd5e6711a01a000eec23d4ba67c10b76ad1228bad9fd0da5fef0694213de18ad8911917358f4f4d7452531358393dc170cfae0d4ce0b66aaf2a147f079e5f4'
